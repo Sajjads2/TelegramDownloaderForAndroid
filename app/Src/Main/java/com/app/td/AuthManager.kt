cb6c2dfd6646a8b794313df82e04421afebabbf7
Sajjads2 <124485449+Sajjads2@users.noreply.github.com>
2026-06-16 16:21:41 +0330

    Update AuthManager.kt

--- app/Src/Main/java/com/app/td/AuthManager.kt
+++ app/Src/Main/java/com/app/td/AuthManager.kt
@@ -2,15 +2,32 @@ package com.app.td
 
 object AuthManager {
 
-    fun startLogin(client: TDClient, phone: String) {
-        client.send(
-            TdApi.SetAuthenticationPhoneNumber(phone, null)
-        ) {}
+    fun handle(update: TdApi.Update) {
+
+        if (update is TdApi.UpdateAuthorizationState) {
+
+            when (val state = update.authorizationState) {
+
+                is TdApi.AuthorizationStateWaitPhoneNumber -> {}
+
+                is TdApi.AuthorizationStateWaitCode -> {}
+
+                is TdApi.AuthorizationStateWaitPassword -> {}
+
+                is TdApi.AuthorizationStateReady -> {}
+            }
+        }
     }
 
-    fun sendCode(client: TDClient, code: String) {
-        client.send(
-            TdApi.CheckAuthenticationCode(code)
-        ) {}
+    fun setPhone(client: TDClient, phone: String) {
+        client.send(TdApi.SetAuthenticationPhoneNumber(phone, null))
+    }
+
+    fun setCode(client: TDClient, code: String) {
+        client.send(TdApi.CheckAuthenticationCode(code))
+    }
+
+    fun setPassword(client: TDClient, pass: String) {
+        client.send(TdApi.CheckAuthenticationPassword(pass))
     }
 }
