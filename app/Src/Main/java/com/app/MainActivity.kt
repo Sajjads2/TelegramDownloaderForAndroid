d2becd0b6047568306965fbf39c69b7e198966ad
Sajjads2 <124485449+Sajjads2@users.noreply.github.com>
2026-06-16 15:51:57 +0330

    Update MainActivity.kt

--- app/Src/Main/java/com/app/MainActivity.kt
+++ app/Src/Main/java/com/app/MainActivity.kt
@@ -1,1 +1,18 @@
 
+package com.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.app.ui.LoginScreen
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            LoginScreen()
+        }
+    }
+}
