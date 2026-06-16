e248494d51223cf6688e3d87760ea4e0989f13b6
Sajjads2 <124485449+Sajjads2@users.noreply.github.com>
2026-06-16 16:28:07 +0330

    Create HomeScreen.kt

---
+++ app/Src/Main/java/com/app/ui/HomeScreen.kt
@@ -0,0 +1,21 @@
+package com.app.ui
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+
+@Composable
+fun HomeScreen() {
+
+    Column {
+
+        Text("TDLib Downloader")
+
+        Button(onClick = {}) {
+            Text("Scan")
+        }
+
+        Button(onClick = {}) {
+            Text("Download")
+        }
+    }
+}
